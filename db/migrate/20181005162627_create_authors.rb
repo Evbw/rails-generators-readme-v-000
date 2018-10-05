5e7d3083b20856e2d91534fac2ea86d71386bd8a
Everett <everett.bowline@gmail.com>
2018-10-05 16:34:51 +0000

    automatically backed up by learn

--- db/migrate/20181005162627_create_authors.rb
+++ db/migrate/20181005162627_create_authors.rb
@@ -2,7 +2,7 @@ class CreateAuthors < ActiveRecord::Migration
   def change
     create_table :authors do |t|
       t.string :name
-      t.String :genre
+      t.string :genre
       t.text :bio
 
       t.timestamps null: false
